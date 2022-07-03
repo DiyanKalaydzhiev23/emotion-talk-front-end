55a0bc387f9680870f96dcaadad0c6b9a7fce24a
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-07-03 20:25:54 +0300

    Made App component functional

--- src/App.js
+++ src/App.js
@@ -1,59 +1,55 @@
 import './App.css';
 
-import React, { Component } from 'react'
- 
+import React, { useState } from 'react'
+
 import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
 import { sendRecording } from './services/emotionTalkRequest';
 
- 
-class App extends Component {
-  constructor(props) {
-    super(props)
- 
-    this.state = {
-      recordState: null
+
+function App() {
+
+    const [audioState, setAudioState] = useState({
+        recordState: null
+    });
+
+    const start = () => {
+        setAudioState({
+            recordState: RecordState.START
+        })
     }
-  }
- 
-  start = () => {
-    this.setState({
-      recordState: RecordState.START
-    })
-  }
- 
-  stop = () => {
-    this.setState({
-      recordState: RecordState.STOP
-    })
-  }
- 
-  //audioData contains blob and blobUrl
-  onStop = (audioData) => {
-    this.handleSave(audioData)
-  }
 
-  handleSave = async (audioData) => {
-    const audioBlob = await fetch(audioData.url).then((r) => r.blob());
-    const audioFile = new File([audioBlob], 'voice.wav', { type: 'audio/wav' });
-    const formData = new FormData(); // preparing to send to the server
+    const stop = () => {
+        setAudioState({
+            recordState: RecordState.STOP
+        })
+    }
 
-    formData.append('recording', audioFile);  // preparing to send to the server
-    formData.append('owner_id', 1);  // preparing to send to the server
-    sendRecording(formData); // sending to the server
-  };
- 
-  render() {
-    const { recordState } = this.state
- 
+    const onStop = (audioData) => {
+        handleSave(audioData)
+    }
+
+    const handleSave = async (audioData) => {
+        const audioBlob = await fetch(audioData.url).then((r) => r.blob());
+        const audioFile = new File([audioBlob], 'voice.wav', { type: 'audio/wav' });
+
+        const formData = new FormData();
+
+        formData.append('recording', audioFile);
+        formData.append('owner_id', 1);
+
+        sendRecording(formData);
+    };
+
+    const { recordState } = audioState
+
     return (
-      <div>
-        <AudioReactRecorder state={recordState} onStop={this.onStop} />
-        
-        <button onClick={this.start}>Start</button>
-        <button onClick={this.stop}>Stop</button>
-      </div>
-    )
-  }
+        <div>
+            <AudioReactRecorder state={recordState} onStop={onStop} />
+
+            <button onClick={start}>Start</button>
+            <button onClick={stop}>Stop</button>
+        </div>
+    );
 }
 
 export default App;
